c65d17072e84d8927478a4afa5022705f995bf88
Seta-Andy <anguyen@veritone.com>
2021-08-02 23:15:15 +0700

    create main layout

---
+++ src/layouts/mainLayout.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Appbar from '../components/Appbar';
+import Sidebar from '../components/Sidebar';
+import { makeStyles } from '@material-ui/core/styles';
+
+type MainLayoutProps = {
+    children: React.Component
+}
+const useStyles = makeStyles(theme => ({
+    appContainer: {
+        backgroundColor: 'rgb(244, 246, 248)',
+        display: 'flex',
+        height: '100%'
+    },
+    contentContainer: {
+        paddingTop: '64px',
+        paddingLeft: '256px',
+        width: '100%'
+    }
+}))
+
+export default function MainLayout(props: MainLayoutProps) {
+    const classes = useStyles();
+    return (
+        <div className={classes.appContainer}>
+            <Appbar />
+            <Sidebar />
+            <div className={classes.contentContainer}>
+                {props.children}
+            </div>
+        </div>
+    )
+}
