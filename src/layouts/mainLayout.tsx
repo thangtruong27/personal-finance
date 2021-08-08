4332bc7682e709c546088840ea33723b1b100f36
Seta-Andy <anguyen@veritone.com>
2021-08-08 15:00:13 +0700

    add mainLayout

--- src/layouts/mainLayout.tsx
+++ src/layouts/mainLayout.tsx
@@ -2,32 +2,31 @@ import React from 'react';
 import Appbar from '../components/Appbar';
 import Sidebar from '../components/Sidebar';
 import { makeStyles } from '@material-ui/core/styles';
+import { RouteProps } from 'react-router-dom';
 
-type MainLayoutProps = {
-    children: React.Component
+export interface MainLayoutProps {
+  children: React.ReactElement;
 }
-const useStyles = makeStyles(theme => ({
-    appContainer: {
-        backgroundColor: 'rgb(244, 246, 248)',
-        display: 'flex',
-        height: '100%'
-    },
-    contentContainer: {
-        paddingTop: '64px',
-        paddingLeft: '256px',
-        width: '100%'
-    }
-}))
+const useStyles = makeStyles((theme) => ({
+  appContainer: {
+    backgroundColor: 'rgb(244, 246, 248)',
+    display: 'flex',
+    height: '100%',
+  },
+  contentContainer: {
+    paddingTop: '64px',
+    paddingLeft: '256px',
+    width: '100%',
+  },
+}));
 
 export default function MainLayout(props: MainLayoutProps) {
-    const classes = useStyles();
-    return (
-        <div className={classes.appContainer}>
-            <Appbar />
-            <Sidebar />
-            <div className={classes.contentContainer}>
-                {props.children}
-            </div>
-        </div>
-    )
+  const classes = useStyles();
+  return (
+    <div className={classes.appContainer}>
+      <Appbar />
+      <Sidebar />
+      <div className={classes.contentContainer}>{props.children}</div>
+    </div>
+  );
 }
