6a903f45df9554f68b4d65973aaf5a0f481cd4fd
Seta-Andy <anguyen@veritone.com>
2021-09-03 22:32:03 +0700

    install xlsx

--- src/state/app/import/index.ts
+++ src/state/app/import/index.ts
@@ -1,2 +1,18 @@
-export { namespace } from "./actions";
-export { default as reducer } from "./reducer";
+export { namespace } from './actions';
+export { default as reducer } from './reducer';
+
+export enum ExpenseCategory {
+  Rent = 'Rent', // fixed monthly rent.
+  Food = 'Food',
+  Transporttation = 'Transporttation',
+  Utilities = 'Utilities', // includes electricity, internet, phone.
+  HealthMaintaince = 'HealthMaintaince',
+  Entertainment = 'Entertainment', // includes movies, traveling, monthly subscription.
+  Education = 'Education',
+  Other = 'Other',
+}
+export interface Data {
+  date: string;
+  amount: string;
+  type: ExpenseCategory;
+}
--- src/state/app/import/saga.ts
+++ src/state/app/import/saga.ts
@@ -1,7 +1,31 @@
 import { takeEvery } from 'redux-saga/effects';
 import { actionTypes, importAction } from './actions';
+import { read, WorkBook, utils } from 'xlsx';
 
-function* importFileSaga(action: importAction) {}
+function processData(data: any[]) {
+  // TODO: process data
+}
+
+function* importFileSaga(action: importAction) {
+  const { fileRef } = action.payload;
+  if (!fileRef) {
+    return;
+  }
+  const fileReader = new FileReader();
+  fileReader.readAsBinaryString(fileRef);
+
+  fileReader.onload = (e) => {
+    const data = e.target?.result;
+    const workbook: WorkBook = read(data, { type: 'binary' });
+    const fileData: any[] = [];
+    workbook.SheetNames.forEach((name) => {
+      const rowData = utils.sheet_to_json(workbook.Sheets[name]);
+      fileData.push(rowData);
+    });
+    const normalizedData = processData(fileData);
+    // TODO save data
+  };
+}
 
 export default function* importWatcher() {
   yield takeEvery(actionTypes.IMPORT_FILE, importFileSaga);
