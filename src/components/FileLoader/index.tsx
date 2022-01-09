17bfc1f431dfa27a514f8122c25c5b4157b0e72c
Truong Nguyen Thang <truongnt0279@gmail.com>
2022-01-09 15:18:23 +0700

    redesign file loader

--- src/components/FileLoader/index.tsx
+++ src/components/FileLoader/index.tsx
@@ -1,41 +1,26 @@
 import React, { useRef } from 'react';
-import { makeStyles, Typography } from '@material-ui/core';
+import { makeStyles, Typography, Card, Button } from '@material-ui/core';
 import { CloudUpload as CloudUploadIcon } from '@material-ui/icons';
+import { Folder as FolderIcon } from '@material-ui/icons';
 import { useDispatch } from 'react-redux';
 import { importFile } from '../../state/app/import/actions';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     backgroundColor: '#fff',
-    padding: '37px 53px',
+    padding: theme.spacing(3),
     width: '470px',
   },
-  title: {},
-  description: {
-    color: 'rgba(0,0,0,0.54)',
-    marginTop: '5px',
-    maxWidth: '350px',
-  },
-  fileReq: {
-    marginTop: '12px',
-    color: 'rgba(0,0,0,0.54)',
-    fontSize: '12px',
-  },
-  example: {
-    color: '#2196F3',
-    marginLeft: '20px',
+  title: {
+    maxWidth: '260px',
+    textAlign: 'center',
   },
   icon: {
     height: '50px',
     width: '50px',
-    color: ' #9E9E9E',
   },
   uploadArea: {
-    width: '350px',
-    height: '140px',
     border: '2px dashed #E0E0E0',
-    borderRadius: '2px',
-    marginTop: '5px',
     '&:hover': {
       cursor: 'pointer',
     },
@@ -43,9 +28,12 @@ const useStyles = makeStyles((theme) => ({
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
   },
-  uploadLabel: {
-    color: ' #229DF8',
+  divider: {
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
   },
 }));
 
@@ -63,24 +51,20 @@ export default function FileLoader() {
     seletedFiles && dispatch(importFile({ fileRef: seletedFiles[0] }));
   };
   return (
-    <div className={classes.root}>
-      <Typography className={classes.title} variant="h2">
-        Upload data
-      </Typography>
-      <Typography className={classes.description} variant="body2">
-        Import daily personal consumption expenditures data to analytic
-      </Typography>
-      <Typography variant="body2" className={classes.fileReq}>
-        File requirements: excels, max file size of 10MB
-        <a className={classes.example}>See example</a>
-      </Typography>
+    <Card className={classes.root}>
       <div className={classes.uploadArea} onClick={handleTriggerUploadFile}>
-        <CloudUploadIcon className={classes.icon} />
-        <Typography variant="h6" className={classes.uploadLabel}>
-          Browse to upload
+        <FolderIcon className={classes.icon} color="primary" />
+        <Typography className={classes.title} variant="h5">
+          Drag your document here(.xls) to start analyzing.
         </Typography>
+        <Typography variant="body1" color="textSecondary" className={classes.divider}>
+          OR
+        </Typography>
+        <Button variant="contained" color="primary">
+          Browse file
+        </Button>
         <input type="file" hidden ref={inputEl} onChange={handleUploadFile} />
       </div>
-    </div>
+    </Card>
   );
 }
